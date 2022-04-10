d2338aafcc88f6ed4a83c615ee04cc9290f3d8b6
HuguesBriqueler <hugues.briqueler@gmail.com>
2022-04-10 04:43:06 +0200

    Added logic for ADDITEM action in Redux reducer

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -6,10 +6,26 @@ export default function reducer(state = INITIAL_STATE, action) {
   switch (action.type) {
     case actions.ADDITEM:
       console.log("ADDITEM");
-      return {
-        ...state,
-        [action.payload.id]: action.payload,
-      };
+      const itemIndex = state.cart.findIndex(
+        (item) => item.id === action.payload.id
+      );
+      if (itemIndex === -1) {
+        return {
+          cart: [...state.cart, action.payload],
+        };
+      } else {
+        return {
+          cart: [
+            ...state.cart.slice(0, itemIndex),
+            {
+              ...state.cart[itemIndex],
+              quantity:
+                state.cart[itemIndex].quantity + action.payload.quantity,
+            },
+            ...state.cart.slice(itemIndex + 1),
+          ],
+        };
+      }
     case actions.REMOVEITEM:
       return {
         ...state,
