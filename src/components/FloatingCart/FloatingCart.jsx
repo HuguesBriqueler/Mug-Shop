3ecec0382e9f39e12fa923c02ae44b66f70136db
HuguesBriqueler <hugues.briqueler@gmail.com>
2022-04-11 11:06:03 +0200

    Displayed number of items in cart next to cart icon

--- src/components/FloatingCart/FloatingCart.jsx
+++ src/components/FloatingCart/FloatingCart.jsx
@@ -1,15 +1,22 @@
 import cartIcon from "./shopping-cart.svg";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 import styles from "./FloatingCart.module.css";
 
 export default function FloatingCart() {
+  // We retrieve the cart from the Redux store
+  const cart = useSelector((state) => state.cart);
+  // then we calculate the number of items in the cart to display it next to the cart icon
+  const totalItems = cart.reduce((acc, item) => {
+    return acc + item.quantity;
+  }, 0);
   return (
-    <Link to="shoppingcart">
+    <Link to="/shoppingcart">
       <div className={styles.floatingCart}>
         <p>Votre panier</p>
         <div className={styles.cartNotifContainer}>
           <img src={cartIcon} alt="Cart icon" />
-          <span className={styles.notif}>0</span>
+          <span className={styles.notif}>{totalItems}</span>
         </div>
       </div>
     </Link>
