e5919418222c06b91b1b60097566d22c01d5b2b5
HuguesBriqueler <hugues.briqueler@gmail.com>
2022-04-11 18:55:38 +0200

    Added total price + payment button

--- src/pages/ShoppingCart/ShoppingCart.jsx
+++ src/pages/ShoppingCart/ShoppingCart.jsx
@@ -19,6 +19,10 @@ export default function ShoppingCart() {
     }
   };
 
+  const totalPrice = cart.reduce((acc, item) => {
+    return acc + item.price * item.quantity;
+  }, 0);
+
   return (
     <div className="global-container">
       <p className={styles.headingCart}></p>
@@ -45,6 +49,8 @@ export default function ShoppingCart() {
           </li>
         ))}
       </ul>
+      <p className={styles.total_price}>Total: {totalPrice.toFixed(2)}€</p>
+      <button className={styles.btn_cart}>Paiement</button>
     </div>
   );
 }
